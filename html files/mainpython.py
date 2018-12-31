2aedc833dcf5b1a465d8c96dac2b6073b4a9e4cb
rms-sth <37908014+rms-sth@users.noreply.github.com>
2018-12-31 20:23:59 +0545

    ramesh deleted python file

--- html files/mainpython.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-print ("hwoo wordkfhsdkfhdskh")
-print ("ramesh Pradhan 4th edit")
-=======
-print ("hwoo wordkfhsdkfhdskh")
-print("bhuwan pandeya")
->>>>>>> b6151043ce553ecac86be57a7d33bbafc3c9fcf9
